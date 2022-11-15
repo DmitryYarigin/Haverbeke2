5dbacd490f480711fcbf8e7fb937b9e39042b7e8
unknown <yarich9999@gmail.com>
2022-11-15 10:18:30 +0300

    feat: 1 and 2 chapters

---
+++ script.js
@@ -0,0 +1,26 @@
+// let myNumber = 5 % 3
+// console.log(myNumber);
+
+// console.log(- (10 -2));
+// console.log(3 > 2);
+// console.log("Хочется" != "Колется");
+
+// console.log(true && false);
+// console.log(true || false);
+// console.log(!true);
+// console.log(false ? 1 : 2);
+// console.log("5" - 1);
+
+// let luigisDebt = 140;
+// luigisDebt = luigisDebt - 34;
+// console.log(luigisDebt);
+
+// let one = 1, two = 2;
+// console.log(one + two);
+
+// prompt("Введите пароль")
+
+console.log(Math.min(2,4) + 100);
+
+let theNumber = Number(prompt("введите число"));
+console.log("это число является квадратным корнем из " + theNumber * theNumber);
